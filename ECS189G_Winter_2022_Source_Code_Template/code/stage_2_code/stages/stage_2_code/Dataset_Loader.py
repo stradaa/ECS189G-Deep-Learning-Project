0721bece72d209171b0778d24eef6ecd1b0cd6bc
Sean H <seanescan@gmail.com>
2022-01-27 19:46:51 -0800

    allow 2 files to be used

--- ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/stages/stage_2_code/Dataset_Loader.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/stages/stage_2_code/Dataset_Loader.py
@@ -12,21 +12,35 @@ class Dataset_Loader(dataset):
     data = None
     dataset_source_folder_path = None
     dataset_source_file_name = None
-    
+    dataset_source_file_test_name = None
+
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
-    
+
     def load(self):
         print('loading data...')
         X = []
         y = []
-        f = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'r')
-        for line in f:
+        X_test = []
+        y_test = []
+        f1 = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'r')
+        f2 = open(self.dataset_source_folder_path + self.dataset_source_file_test_name, 'r')
+        for line in f1:
             line = line.strip('\n')
 
             elements = [int(i) for i in line.split()]
 
             X.append(elements[1:])
             y.append(elements[0])
-        f.close()
-        return {'X': X, 'y': y}
+
+        for line in f2:
+            line = line.strip('\n')
+
+            elements = [int(i) for i in line.split()]
+
+            X_test.append(elements[1:])
+            y_test.append(elements[0])
+
+        f1.close()
+        f2.close()
+        return {'X': X, 'X_test': X_test, 'y': y, 'y_test': y_test}
--- ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/stages/stage_2_code/Setting_KFold_CV.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/stages/stage_2_code/Setting_KFold_CV.py
@@ -17,11 +17,27 @@ class Setting_KFold_CV(setting):
         # load dataset
         loaded_data = self.dataset.load()
         
-        kf = KFold(n_splits=self.fold, shuffle=True)
-        
-        fold_count = 0
+        # kf = KFold(n_splits=self.fold, shuffle=True)
+
+        # fold_count = 0
         score_list = []
-        for train_index, test_index in kf.split(loaded_data['X']):
+        X_train, X_test = np.array(loaded_data['X']), np.array(loaded_data['X_test'])
+        y_train, y_test = np.array(loaded_data['y']), np.array(loaded_data['y_test'])
+
+        # run MethodModule
+        self.method.data = {'train': {'X': X_train, 'y': y_train}, 'test': {'X': X_test, 'y': y_test}}
+        learned_result = self.method.run()
+
+        # save raw ResultModule
+        self.result.data = learned_result
+        self.result.save()
+
+        self.evaluate.data = learned_result
+        score_list.append(self.evaluate.evaluate())
+        return np.mean(score_list), np.std(score_list)
+
+        """ 
+       for train_index, test_index in kf.split(loaded_data['X']):
             fold_count += 1
             print('************ Fold:', fold_count, '************')
             X_train, X_test = np.array(loaded_data['X'])[train_index], np.array(loaded_data['X'])[test_index]
@@ -38,7 +54,6 @@ class Setting_KFold_CV(setting):
             
             self.evaluate.data = learned_result
             score_list.append(self.evaluate.evaluate())
-        
-        return np.mean(score_list), np.std(score_list)
+        """
 
         
