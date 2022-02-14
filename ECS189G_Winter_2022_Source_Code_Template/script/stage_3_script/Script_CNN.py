11af91f437b35eaa0fefdc99fcffeec944132fa0
mjpitts <mjpitts@ucdavis.edu>
2022-02-13 20:48:46 -0800

    what I have rn

--- ECS189G_Winter_2022_Source_Code_Template/script/stage_3_script/Script_CNN.py
+++ ECS189G_Winter_2022_Source_Code_Template/script/stage_3_script/Script_CNN.py
@@ -1,16 +1,16 @@
 from stages.stage_3_code.Dataset_Loader import Dataset_Loader
 from stages.stage_3_code.Method_CNN import Method_CNN
+from stages.stage_3_code.Method_CNN_ORL import Method_CNN_ORL
 from stages.stage_3_code.Result_Saver import Result_Saver
-from stages.stage_3_code.Setting_KFold_CV import Setting_KFold_CV
 from stages.stage_3_code.Setting_TrainTest import TrainTestSplit
+from stages.stage_3_code.Setting_TrainTest_ORL import TrainTestSplit_ORL
 from stages.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
 import numpy as np
 import torch
-from PIL import Image as im
 
+control = input("chose your dataset. 1 for MNIST, 2 for ORL, and 3 for CIFAR: ")
 
-
-if 1:
+if control == "1":
     # ---- parameter section -------------------------------
     np.random.seed(2)
     torch.manual_seed(2)
@@ -47,8 +47,52 @@ if 1:
     setting_obj.print_setup_summary()
 
     metrics = setting_obj.load_run_save_evaluate()
-    # print('************ Overall Performance ************')
-    # print('MLP Metrics are as follows: ')
-    # print(' Accuracy, Precision, Recall, F1 ... ' + str(metrics))
-    # print('************ Finish ************')
+    print('************ Overall Performance ************')
+    print('CNN Metrics are as follows: ')
+    print(' Accuracy, Precision, Recall, F1 ... ' + str(metrics))
+    print('************ Finish ************')
+    #------------------------------------------------------
+
+if control == "2":
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
     # ------------------------------------------------------
+
+    # ---- objection initialization section ---------------
+    data_obj = Dataset_Loader('stage_3_data', '')
+    data_obj.dataset_source_folder_path = '../../data/stage_3_data/'
+
+    data_obj.dataset_source_file_name = 'ORL'
+
+    data = data_obj.load()
+    train = data['train']
+    test = data['test']
+
+    print("here")
+
+    method_obj = Method_CNN_ORL('CNN', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_3_result/MLP_'
+    result_obj.result_destination_file_name = 'prediction_result'
+
+
+    setting_obj = TrainTestSplit_ORL('Train Test Split', '')
+    # setting_obj = Setting_Tra
+    # in_Test_Split('train test split', '')
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+
+    metrics = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('CNN Metrics are as follows: ')
+    print(' Accuracy, Precision, Recall, F1 ... ' + str(metrics))
+    print('************ Finish ************')
+    #------------------------------------------------------
