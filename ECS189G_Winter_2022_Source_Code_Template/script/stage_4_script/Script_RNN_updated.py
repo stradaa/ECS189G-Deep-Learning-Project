f03239e706907e0062a194fe92d7827c9ddb20a2
stradaa <estradaa6@gmail.com>
2022-02-27 18:45:16 -0800

    fixed word_embedding

---
+++ ECS189G_Winter_2022_Source_Code_Template/script/stage_4_script/Script_RNN_updated.py
@@ -0,0 +1,97 @@
+from stages.stage_4_code.Dataset_Loader import Dataset_Loader
+from stages.stage_4_code.Method_RNN2 import RNN
+import numpy as np
+import pickle
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+import time
+
+
+if 1:
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    # ------------------------------------------------------
+
+    # ---- objection initialization section ---------------
+    data_obj_train = Dataset_Loader('stage_4_data')
+    data_obj_train.dataset_source_folder_path = r'C:/Users/Alex Estrada/PycharmProjects/ECS189G/' \
+                                                r'ECS189G_Winter_2022_Source_Code_Template/' \
+                                                r'data/stage_4_data/text_classification/'
+    data_obj_train.dataset_source_file_name = ''
+
+    # data = data_obj_train.load2()
+
+    train_dict = pickle.load(open('train_pickle', 'rb'))
+    test_dict = pickle.load(open('test_pickle', 'rb'))
+
+    word_embedding_results = data_obj_train.word_embedding(train_dict, test_dict)
+
+    INPUT_DIM = len(word_embedding_results[0].vocab)
+    EMBEDDING_DIM = 100
+    HIDDEN_DIM = 256
+    OUTPUT_DIM = 1
+    N_LAYERS = 2
+    BIDIRECTIONAL = True
+    DROPOUT = 0.5
+    PAD_IDX = word_embedding_results[0].vocab.stoi[word_embedding_results[0].pad_token]
+
+    model = RNN(INPUT_DIM,
+                EMBEDDING_DIM,
+                HIDDEN_DIM,
+                OUTPUT_DIM,
+                N_LAYERS,
+                BIDIRECTIONAL,
+                DROPOUT,
+                PAD_IDX)
+
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'The model has {count_parameters(model):,} trainable parameters')
+
+    pretrained_embeddings = word_embedding_results[0].vocab.vectors
+    print(pretrained_embeddings.shape)
+
+    model.embedding.weight.data.copy_(pretrained_embeddings)
+
+    UNK_IDX = word_embedding_results[0].vocab.stoi[word_embedding_results[0].unk_token]
+    model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+    model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+    print(model.embedding.weight.data)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # train the model
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.BCEWithLogitsLoss()
+
+    model = model.to(device)
+    criterion = criterion.to(device)
+
+    N_EPOCHS = 5
+
+    best_valid_loss = float('inf')
+
+    for epoch in range(N_EPOCHS):
+        print("START TRAINING")
+        start_time = time.time()
+
+        train_loss, train_acc = RNN.train(model, word_embedding_results[2], optimizer, criterion)
+        valid_loss, valid_acc = RNN.evaluate(model, word_embedding_results[3], criterion)
+
+        end_time = time.time()
+
+        epoch_mins, epoch_secs = RNN.epoch_time(start_time, end_time)
+
+        print("VALID_LOSS:", valid_loss, type(valid_loss))
+        print("BEST LOSS:", best_valid_loss, type(best_valid_loss))
+
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), 'tut1-model.pt')
+
+        print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+        print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
---
+++ ECS189G_Winter_2022_Source_Code_Template/stages/stage_4_code/Method_RNN2.py
@@ -0,0 +1,116 @@
+import torch.nn as nn
+import torch
+import time
+
+
+class RNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers,
+                 bidirectional, dropout, pad_idx):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+
+        self.rnn = nn.LSTM(embedding_dim,
+                           hidden_dim,
+                           num_layers=n_layers,
+                           bidirectional=bidirectional,
+                           dropout=dropout)
+
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text, text_lengths):
+        # text = [sent len, batch size]
+
+        embedded = self.dropout(self.embedding(text))
+
+        # embedded = [sent len, batch size, emb dim]
+
+        # pack sequence
+        # lengths need to be on CPU!
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths, batch_first=True)
+
+        packed_output, (hidden, cell) = self.rnn(packed_embedded)
+
+        # unpack sequence
+        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
+
+        # output = [sent len, batch size, hid dim * num directions]
+        # output over padding tokens are zero tensors
+
+        # hidden = [num layers * num directions, batch size, hid dim]
+        # cell = [num layers * num directions, batch size, hid dim]
+
+        # concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
+        # and apply dropout
+
+        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
+
+        # hidden = [batch size, hid dim * num directions]
+
+        return self.fc(hidden)
+
+    def binary_accuracy(preds, y):
+        """
+        Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+        """
+
+        # round predictions to the closest integer
+        rounded_preds = torch.round(torch.sigmoid(preds))
+        correct = (rounded_preds == y).float()  # convert into float for division
+        acc = correct.sum() / len(correct)
+        return acc
+
+    def train(model, iterator, optimizer, criterion):
+        epoch_loss = 0
+        epoch_acc = 0
+
+        for batch in iterator:
+            optimizer.zero_grad()
+
+            text = batch.words
+            text_lengths = torch.as_tensor([64, 1], dtype=torch.int64, device='cpu')
+
+            predictions = model(text, text_lengths).squeeze(1)
+
+            loss = criterion(predictions, batch.Label)
+
+            acc = binary_accuracy(predictions, batch.Label)
+
+            loss.backward()
+
+            optimizer.step()
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+        return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+    def evaluate(model, iterator, criterion):
+
+        epoch_loss = 0
+        epoch_acc = 0
+
+        # model.eval()
+
+        with torch.no_grad():
+            for batch in iterator:
+                text, text_lengths = batch.words
+
+                predictions = model(text, text_lengths).squeeze(1)
+
+                loss = criterion(predictions, batch.Label)
+
+                acc = binary_accuracy(predictions, batch.Label)
+
+                epoch_loss += loss.item()
+                epoch_acc += acc.item()
+
+        return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+    def epoch_time(start_time, end_time):
+        elapsed_time = end_time - start_time
+        elapsed_mins = int(elapsed_time / 60)
+        elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+        return elapsed_mins, elapsed_secs
